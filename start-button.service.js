0d65e9769fdf0541664bf80f36137bd025b97271
Daniel DRaffer <daniel.raffer@gmail.com>
2022-03-11 16:45:16 +0100

    add start button service

---
+++ start-button.service.js
@@ -0,0 +1,33 @@
+export class StartButtonService {
+    #startButton;
+
+    setDisabled = (isDisabled) => {
+        if(isDisabled){
+            this.#startButton.setAttribute('disabled', isDisabled);
+            return;
+        };
+        this.#startButton.removeAttribute('disabled');
+    };
+
+    setToStart = () => {
+        this.#startButton.innerText = 'Start';
+        this.#startButton.classList.remove('btn-danger');
+    };
+
+    setToPause = () => {
+        this.startButton.innerText = 'Pause';
+        this.startButton.classList.add('btn-danger');
+    };
+
+    blur = () => {
+        this.#startButton.blur();
+    };
+
+    constructor(appStateSetter){
+        this.#startButton = document.getElementById('start_pause');
+        this.#startButton.addEventListener('click', () => {
+            //this.setRun(!this.run);
+            this.blur();
+        })
+    };
+};
